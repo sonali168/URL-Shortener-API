52f6a9ef5ced64a0651459f3df648e6e839a3486
Sonali Nachankaar <80123687+sonali168@users.noreply.github.com>
2024-11-27 11:24:31 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const app = require('./app');
+const express = require('express');
+
 
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the URL Shortener API!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
